1cd34b039093aa20461c21e9c0c40a84169afa2a
Berkay <berkaydemirbas@yahoo.com>
2020-07-22 01:28:40 +0300

    Uncommented API

--- src/pages/videos.js
+++ src/pages/videos.js
@@ -1,52 +1,52 @@
-// import React from 'react';
-// import { Layout, SEO } from 'components';
-// import { graphql } from "gatsby"
+import React from 'react';
+import { Layout, SEO } from 'components';
+import { graphql } from "gatsby"
 
-// export default class Videos extends React.Component {
-//   render() {
-//     const videos = this.props.data.allYoutubeVideo
-//     return (
-//       <Layout>
-//         <SEO title='Videos' />
-//         <div className='video-grid'>
-//           {
-//             videos.edges.map(({ node }) => (
-//               <div className='video-item' key={node.id}>
-//                 <a href={`https://www.youtube.com/watch?v=${node.videoId}`} rel="noopener noreferrer" target='_blank' >
-//                   <div>
-//                   <img className='video-card' src={node.thumbnail.url} alt={node.title} />
-//                   <div className='video-description'>
-//                   <h1>{node.title}</h1>
-//                   <date>{node.publishedAt}</date>
-//                   </div>
-//                   </div>
-//                 </a>
-//               </div>
-//             ))
-//           }
-//         </div>
-//       </Layout>
-//     )
-//   }
-// }
+export default class Videos extends React.Component {
+  render() {
+    const videos = this.props.data.allYoutubeVideo
+    return (
+      <Layout>
+        <SEO title='Videos' />
+        <div className='video-grid'>
+          {
+            videos.edges.map(({ node }) => (
+              <div className='video-item' key={node.id}>
+                <a href={`https://www.youtube.com/watch?v=${node.videoId}`} rel="noopener noreferrer" target='_blank' >
+                  <div>
+                  <img className='video-card' src={node.thumbnail.url} alt={node.title} />
+                  <div className='video-description'>
+                  <h1>{node.title}</h1>
+                  <date>{node.publishedAt}</date>
+                  </div>
+                  </div>
+                </a>
+              </div>
+            ))
+          }
+        </div>
+      </Layout>
+    )
+  }
+}
 
-// export const YoutubeVideosQuery = graphql`
-// query YoutubeVideosQuery {
-//   allYoutubeVideo {
-//     edges {
-//       node {
-//         id
-//         publishedAt(fromNow: true)
-//         title
-//         thumbnail {
-//           url
-//           height
-//           width
-//         }
-//         description
-//         videoId
-//       }
-//     }
-//   }
-// }
-// `
+export const YoutubeVideosQuery = graphql`
+query YoutubeVideosQuery {
+  allYoutubeVideo {
+    edges {
+      node {
+        id
+        publishedAt(fromNow: true)
+        title
+        thumbnail {
+          url
+          height
+          width
+        }
+        description
+        videoId
+      }
+    }
+  }
+}
+`
